ca9a73cd1d29ad1b6b04650e7ad44dc1a375d07f
abdo1917 <102588811+abdo1917@users.noreply.github.com>
2024-07-17 08:58:50 +0300

    init app

---
+++ lib/Colors.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+class MyColors{
+  static const Color backgroundGame= Color(0xFF3F4E6B);
+  static const Color backgroundResult= Color(0xFF3D4C68);
+  static const Color button= Color(0xFF899CCD);
+  static const Color textHome = Color(0xFFC9C6B1);
+  static const Color colorO= Color(0xFFFF9B8D);
+  static const Color colorX= Color(0xFFFFFFFF);
+  static const Color cardColor= Color(0xFF1F273E);
+}
---
+++ lib/images.dart
@@ -0,0 +1,16 @@
+
+
+class My_Images{
+
+  static const String  person= 'assets/images/person.png';
+  static const String  people= 'assets/images/people.png';
+  static const String  icon= 'assets/images/icon.png';
+  static const String  wallpaper= 'assets/images/dark_wallpaper.jpeg';
+  static const String reset= 'assets/images/reset.png';
+  static const String won='assets/images/won.png';
+  static const String defeat='assets/images/dislike.png';
+  static const String draw='assets/images/draw.png';
+  static const String ticTac='assets/images/tic-tac.png';
+  static const String ticTacDraw='assets/images/tic-tac-draw.png';
+
+}
